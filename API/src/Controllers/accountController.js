0e9c59f446677f799eb9fffee1d4179e578f46f9
Lucas Manrick <lucasmanrick.ipms@gmail.com>
2024-09-13 19:15:36 -0300

    cliente adicionado com hash

--- API/src/Controllers/accountController.js
+++ API/src/Controllers/accountController.js
@@ -1,6 +1,6 @@
 const Usuario = require("../Models/Class/Usuario")
 const AccountQueries = require("../Models/Query/AccountQueries")
-
+const bcrypt = require('bcrypt');
 
 const accountManagement = {
   cadastraUsuario: async (req,res) =>  {
@@ -11,12 +11,28 @@ const accountManagement = {
       if(NomeUsuario,DataNasc,Email,Senha,Cep,Rua,Numero,Bairro,Estado,Cidade) {
           if (typeof(Email) == 'number') {
             res.json ({error:"O Campo Email não pode ser numero"})
-          }else if (typeof(Cep) !== 'number') {
-            res.json ({error:"O Campo Cep não pode ser texto"})
+          }else if (typeof(NomeUsuario) !== 'string') {
+            res.json ({error:"O Campo Nome não pode ser diferente de texto"})
+          }else if (typeof(Senha) !== 'string') {
+            res.json ({error:"O Campo Senha está com tipo diferente do esperado"})
+          }else if (typeof(Cep) !== 'string') {
+            res.json ({error:"O Campo CEP está com tipo diferente do esperado"})
+          }else if (typeof(Rua) !== 'string') {
+            res.json ({error:"O Campo Rua está com tipo diferente do esperado"})
+          }else if (typeof(Numero) !== 'number') {
+            res.json ({error:"O Campo Numero está com tipo diferente do esperado"})
+          }else if (typeof(Bairro) !== 'string') {
+            res.json ({error:"O Campo Bairro está com tipo diferente do esperado"})
+          }else if (typeof(Estado) !== 'string') {
+            res.json ({error:"O Campo Estado está com tipo diferente do esperado"})
+          }else if (typeof(Cidade) !== 'string') {
+            res.json ({error:"O Campo Cidade está com tipo diferente do esperado"})
           }
 
+
+          const hashedPassword = await bcrypt.hash(Senha,10); // aki ocorre a criptografia da parte da qual queremos, e determinamos que seja 10 apos o campo que queremos criptografar para que 10 mil registros com a mesma senha tenha criptografias diferentes, ou seja se 30 pessoas tiverem a mesma senha as 30 terão criptografias diferentes.
           
-          const newUser = new Usuario(NomeUsuario,DataNasc,Email,Senha,Cep,Rua,Numero,Bairro,Estado,Cidade)
+          const newUser = new Usuario(NomeUsuario,DataNasc,Email,hashedPassword,Cep,Rua,Numero,Bairro,Estado,Cidade)
           //verificar se o Email passado já não existe no nosso banco de dados.
   
           const sendingData = await AccountQueries.AccountManagementQueries.cadastraUsuarioQuery(newUser)
--- API/src/Routes/routesapp.js
+++ API/src/Routes/routesapp.js
@@ -5,6 +5,7 @@ const accountManagement = require ('../Controllers/accountController')
 
 const users = []; //esse users representa o banco de dados, ou seja a tabela onde receberá o dado criptografado, ou melhor, o local onde ficará armazenado a senha no nosso banco de dados.
 
+
 router.post("/Cadastro",accountManagement.cadastraUsuario)
 
 // // rota para o usuário fazer um cadastro no site.
@@ -22,9 +23,9 @@ router.post("/Cadastro",accountManagement.cadastraUsuario)
 
 // app.post ('/users', async (req,res) => { //CRIPTOGRAFANDO ANTES DE CADASTRAR NO BANCO DE DADOS.
 //   try{
-//     const hashedPassword = await bcrypt.hash(req.body.password,10); // aki ocorre a criptografia da parte da qual queremos, e determinamos que seja 10 apos o campo que queremos criptografar para que 10 mil registros com a mesma senha tenha criptografias diferentes, ou seja se 30 pessoas tiverem a mesma senha as 30 terão criptografias diferentes.
-//     const user = {name:req.body.name, password:hashedPassword}; // aqui, apenas organizamos os dados ja tratados da forma com qual devem ir ao banco de dados.
-//     users.push(user); // aki fazemos o envio do usuário 'user' ao banco de dados na tabela 'users'
+//    const user = {name:req.body.name, password:hashedPassword}; // aqui, apenas organizamos os dados ja tratados da forma com qual devem ir ao banco de dados.
+//users.push(user); // aki fazemos o envio do usuário 'user' ao banco de dados na tabela 'users'
+          
 //     res.status(201).send();
 //   }catch(e) {
 //     console.log (e)
