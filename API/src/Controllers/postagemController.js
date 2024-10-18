c49a0447c7c7ba6ca91152438db04190074643d1
Lucas Manrick <lucasmanrick.ipms@gmail.com>
2024-10-18 20:54:18 -0300

    postagem pronto

--- API/src/Controllers/postagemController.js
+++ API/src/Controllers/postagemController.js
@@ -2,18 +2,45 @@ const Postagem = require("../Models/Class/Postagem");
 
 
 const postagemController = {
-  criaPostagem: async (req,res) => {
-    const {ID} = req.dataUser;
-    const {Descricao} = req.body;
+  criaPostagem: async (req, res) => {
+    const { ID } = req.dataUser;
+    const { Descricao } = req.body;
     try {
       const dataPublicacao = new Date();
 
-      if(ID && Descricao && dataPublicacao) {
-        const newPost = new Postagem(null,Descricao,dataPublicacao,ID);
+      if (ID && Descricao && dataPublicacao) {
+        const newPost = new Postagem(null, Descricao, dataPublicacao, ID);
         return res.json(await newPost.criarPostagemQuery());
       }
-    }catch(e) {
-      return res.json({error:e.message})
+    } catch (e) {
+      return res.json({ error: e.message })
+    }
+  },
+
+  editarPostagem: async (req, res) => {
+    const { ID } = req.dataUser;
+    const { IDPostagem, Descricao } = req.body;
+    try {
+      if (ID && IDPostagem && Descricao) {
+        const editPost = new Postagem(IDPostagem, Descricao, null, ID)
+        return res.json(await editPost.editarPostagemQuery())
+      }
+    } catch (e) {
+     return res.json({ error: e.message })
+    }
+  },
+
+
+  verPostagens: async (req,res) => {
+    const {ID} = req.dataUser;
+    const {gapQuantity} = req.body
+    try{
+     return res.json(await Postagem.verPostagensQuery(gapQuantity,ID))
+      // if(quantityGet) {
+       
+      // }
+    }catch(e){
+      res.json({error:e.message})
     }
   }
 }
--- API/src/Models/Class/Postagem.js
+++ API/src/Models/Class/Postagem.js
@@ -26,7 +26,21 @@ class Postagem {
   async editarPostagemQuery () {
     const conn = await connection();
     try {
+      const editingPost = await conn.query("UPDATE postagem SET Descricao = ? IDUsuario =? WHERE ID = ? AND IDUsuario = ?" [this.Descricao,this.IDUsuario,this.ID,this.IDUsuario])
+      if(editingPost[0].affectedRows >=1) {
+        return {success:"dados atualizados com sucesso"}
+      }return {error:"não foi possivel atualizar os dados erro 404"}
+    }catch(e) {
+      return {error:e.message}
+    }
+  }
 
+
+  static async verPostagensQuery (gapQuantity,UserID) {
+    const conn = await connection();
+    try{
+      const getingPosts = await conn.query (`select p.*, u.Nome as NomeUsuario from postagem AS p JOIN contato AS C JOIN Usuario AS U on U.ID = p.IDUsuario WHERE C.IDSolicitante=? AND C.IDDestinatario=p.IDUsuario OR C.IDDestinatario=? AND C.IDSolicitante=p.IDUsuario  ORDER BY dataPublicacao DESC LIMIT 50 OFFSET ${50 * gapQuantity}`,[UserID,UserID]);
+      return{success:"retornando posts de seus amigos",posts:getingPosts[0]}
     }catch(e) {
       return {error:e.message}
     }
