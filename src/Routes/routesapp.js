875e26c061e928c224a70d25fdc9bf17fc1312ca
Lucas Manrick <lucasmanrick.ipms@gmail.com>
2024-12-03 19:52:03 -0300

    logout adjusts

--- src/Routes/routesapp.js
+++ src/Routes/routesapp.js
@@ -2,7 +2,7 @@ require("dotenv-safe").config();
 const jwt = require('jsonwebtoken');
 const express = require('express');
 const router = express.Router();
-const UsuarioController = require ('../Controllers/UsuarioController');
+const UsuarioController = require('../Controllers/UsuarioController');
 const petManagement = require("../Controllers/petController");
 const userInteractController = require("../Controllers/userInteractController");
 const solicitacaoContatoController = require("../Controllers/solicitacaoContatoController");
@@ -21,42 +21,44 @@ const comentarioController = require("../Controllers/comentarioController");
 const { connection } = require("../Config/db");
 
 
-const newDirName = path.resolve(__dirname, '..','..', '..' ,'WEB', 'View','Pages');
+const newDirName = path.resolve(__dirname, '..', '..', '..', 'WEB', 'View', 'Pages');
 
 
-function verificadorDoToken(req, res, next){
+function verificadorDoToken(req, res, next) {
   let token;
 
-  if(req.headers.authorization) {
+  if (req.headers.authorization) {
     token = req.headers['authorization'];
-  }else if (req.cookies.token) {
+  } else if (req.cookies.token) {
     token = req.cookies.token;
   }
 
 
-  
+
   if (!token) return res.status(401).json({ auth: false, message: 'Não foi informado nenhum token.' });
-  
-  jwt.verify(token, process.env.SECRET, function(err, decoded) {
+
+  jwt.verify(token, process.env.SECRET, function (err, decoded) {
     if (err) return res.status(500).json({ auth: false, message: 'Falha ao tentar autenticar o token.' });
-    
+
     // se tudo estiver ok, salva no request para uso posterior
-    req.dataUser = {ID:decoded.ID,Nome:decoded.Nome, CEP:decoded.CEP,Rua: decoded.Rua,
+    req.dataUser = {
+      ID: decoded.ID, Nome: decoded.Nome, CEP: decoded.CEP, Rua: decoded.Rua,
       Numero: decoded.Numero,
       Bairro: decoded.Bairro,
       Estado: decoded.Estado,
       DataNasc: decoded.DataNasc,
       Email: decoded.Email,
       Administrador: decoded.Administrador,
-      Cidade: decoded.Cidade};
+      Cidade: decoded.Cidade
+    };
     next();
   });
 }
 
 
 router.get('/teste', (req, res) => {
-   res.write("testado")
-   res.end();
+  res.write("testado")
+  res.end();
 });
 
 // router.get('/', (req, res) => {
@@ -89,133 +91,139 @@ router.get('/teste', (req, res) => {
 
 
 router.get("/Out", verificadorDoToken, (req, res) => {
- res.cookie('token', undefined, {
-  secure: true, // O cookie só será enviado em conexões HTTP
-  httpOnly: true, // O cookie não será acessível via JavaScript no navegador
-  maxAge: 3600000 // Tempo de vida do cookie em milissegundos (1 hora)
+  res.cookie('perfilSendoVisto', undefined, {
+    secure: true, // O cookie só será enviado em conexões HTTP
   })
- res.end()
+  res.cookie('usuarioLogado', undefined, {
+    secure: true, // O cookie só será enviado em conexões HTTP
+  })
+  res.cookie('token', undefined, {
+    secure: true, // O cookie só será enviado em conexões HTTP
+    httpOnly: true, // O cookie não será acessível via JavaScript no navegador
+    maxAge: 3600000 // Tempo de vida do cookie em milissegundos (1 hora)
+  })
+  res.end()
 })
 
 
 // rota para o usuário fazer um cadastro no site.
-router.post("/Cadastro",UsuarioController.cadastraUsuario)
+router.post("/Cadastro", UsuarioController.cadastraUsuario)
 
 
 //Rota para o usuário logar no site (será retornado um token ao mesmo)
- router.post('/Login' ,UsuarioController.autenticaUsuario) 
+router.post('/Login', UsuarioController.autenticaUsuario)
 
 
 //Rota para cadastrar os dados de um novo pet 
- router.post ("/CadastraPet",verificadorDoToken,petManagement.cadastraPet);
+router.post("/CadastraPet", verificadorDoToken, petManagement.cadastraPet);
 
 
-  //Rota para Editar informações de um pet já cadastrado.
- router.put("/EditaPetInfo", verificadorDoToken,petManagement.editaPetInfo);
+//Rota para Editar informações de um pet já cadastrado.
+router.put("/EditaPetInfo", verificadorDoToken, petManagement.editaPetInfo);
 
 
- //remove um pet da adoção ou inativa (obs: todos registros com status 1 está para adoção, todos registros com status 0 é por que foram doados graças a rede e só inativamos o registro).
- router.delete ("/RetiraPetAdocao",verificadorDoToken,petManagement.removePetDaAdocao)
+//remove um pet da adoção ou inativa (obs: todos registros com status 1 está para adoção, todos registros com status 0 é por que foram doados graças a rede e só inativamos o registro).
+router.delete("/RetiraPetAdocao", verificadorDoToken, petManagement.removePetDaAdocao)
 
 
 
-  //puxa todos animais de um usuário especifico.
-  router.get("/PetsUser/:UserID", verificadorDoToken, petManagement.petsDeUmUsuario)
+//puxa todos animais de um usuário especifico.
+router.get("/PetsUser/:UserID", verificadorDoToken, petManagement.petsDeUmUsuario)
 
 
-  //puxa todos animais que estão para adoção no mesmo estado da pessoa.
-  router.post("/PetsAdocao", verificadorDoToken, petManagement.petsParaAdocao)
+//puxa todos animais que estão para adoção no mesmo estado da pessoa.
+router.post("/PetsAdocao", verificadorDoToken, petManagement.petsParaAdocao)
 
 
-  //Rota que pode-se demonstrar interesse em um pet
-  router.post ("/DemonstrarInteressePet", verificadorDoToken, interesseController.demonstrarInteresseEmPet)
+//Rota que pode-se demonstrar interesse em um pet
+router.post("/DemonstrarInteressePet", verificadorDoToken, interesseController.demonstrarInteresseEmPet)
 
 
-  //Rota para ver todos animais que eu demonstrei interesse
-  router.get ("/InteressadosMeuPet/:MeuPetID", verificadorDoToken, interesseController.interessadosMeuPet)
+//Rota para ver todos animais que eu demonstrei interesse
+router.get("/InteressadosMeuPet/:MeuPetID", verificadorDoToken, interesseController.interessadosMeuPet)
 
 
-  //Rota para ver os pets no qual eu demonstrei interesse
-  router.get ("/MeusInteresses", verificadorDoToken, interesseController.meusInteresses)
+//Rota para ver os pets no qual eu demonstrei interesse
+router.get("/MeusInteresses", verificadorDoToken, interesseController.meusInteresses)
 
 
-  //Rota para tirar interesse em algum pet
-  router.delete ("/RemoverInteressePet", verificadorDoToken, interesseController.removerInteressePet);
+//Rota para tirar interesse em algum pet
+router.delete("/RemoverInteressePet", verificadorDoToken, interesseController.removerInteressePet);
 
 
-  //Rota para Solicitar amizade de um usuário sem que haja interesse em algum de seus pets.
+//Rota para Solicitar amizade de um usuário sem que haja interesse em algum de seus pets.
 
-  router.post ("/SolicitaAmizade", verificadorDoToken, solicitacaoContatoController.enviarSolicitacaoDeAmizade)
-
-
-  //Rota para remover uma solicitação de amizade que enviamos a algum usuário.
-  router.delete ("/RemoveSolicitacao", verificadorDoToken, solicitacaoContatoController.removeSolicitacaoDeAmizade)
-
- //Rota para ver solicitações de amizades enviadas para mim
-  router.get("/MinhasSolicitacoes",verificadorDoToken,solicitacaoContatoController.minhasSolicitacoes);
-
-  //Rota para aceitar uma solicitação de amizade enviada para o usuário logado
-  router.post("/AceitaSolicitacaoAmizade",verificadorDoToken,solicitacaoContatoController.aceitaSolicitacao)
-
-  //Rota para tirar um outro usuário da lista de contatos do usuário logado.
-  router.delete("/RemoveUsuarioDaListaDeContatos",verificadorDoToken,contatoController.removeUsuarioDaListaDeContatos)
-
-  //Rota para obter dados de perfil de usuários do site (chamar quando quiser ver o perfil de um usuário, ou ver o seu próprio perfil).
-  router.get("/ProfileUser/:userBeReturnedID",verificadorDoToken,userInteractController.profileUser);
-
-  //Rota para trazer meus proprios dados de perfil
-  router.get("/MyProfile",verificadorDoToken,userInteractController.profileUser);
-
-  //Rota para o ADM Remover um usuário do sistema, ou o próprio usuário se remover do sistema permanentemente
-  router.delete("/RemoveDadosUsuario",verificadorDoToken,UsuarioController.removeDadosUsuario);
-
-  //Rota para o Usuário conseguir alterar seus dados cadastrais
-  router.put("/EditaDados",verificadorDoToken, UsuarioController.editaDadosCadastrais);
+router.post("/SolicitaAmizade", verificadorDoToken, solicitacaoContatoController.enviarSolicitacaoDeAmizade)
 
 
-  //Rota para puxar dados de todos em minha lista de contato
-  router.get("/MeusContatos",verificadorDoToken, contatoController.meusContatos)
+//Rota para remover uma solicitação de amizade que enviamos a algum usuário.
+router.delete("/RemoveSolicitacao", verificadorDoToken, solicitacaoContatoController.removeSolicitacaoDeAmizade)
 
-  //Rota para puxar as mensagens que tiveram entre o contato selecionado.
-  router.get("/MensagensContato/:IDContato",verificadorDoToken,mensagemController.mensagensContato);
+//Rota para ver solicitações de amizades enviadas para mim
+router.get("/MinhasSolicitacoes", verificadorDoToken, solicitacaoContatoController.minhasSolicitacoes);
+
+//Rota para aceitar uma solicitação de amizade enviada para o usuário logado
+router.post("/AceitaSolicitacaoAmizade", verificadorDoToken, solicitacaoContatoController.aceitaSolicitacao)
+
+//Rota para tirar um outro usuário da lista de contatos do usuário logado.
+router.delete("/RemoveUsuarioDaListaDeContatos", verificadorDoToken, contatoController.removeUsuarioDaListaDeContatos)
+
+//Rota para obter dados de perfil de usuários do site (chamar quando quiser ver o perfil de um usuário, ou ver o seu próprio perfil).
+router.get("/ProfileUser/:userBeReturnedID", verificadorDoToken, userInteractController.profileUser);
+
+//Rota para trazer meus proprios dados de perfil
+router.get("/MyProfile", verificadorDoToken, userInteractController.profileUser);
+
+//Rota para o ADM Remover um usuário do sistema, ou o próprio usuário se remover do sistema permanentemente
+router.delete("/RemoveDadosUsuario", verificadorDoToken, UsuarioController.removeDadosUsuario);
+
+//Rota para o Usuário conseguir alterar seus dados cadastrais
+router.put("/EditaDados", verificadorDoToken, UsuarioController.editaDadosCadastrais);
 
 
-  //Rota para enviar uma mensagem para o contato selecionado
-  router.post("/EnviaMensagem",verificadorDoToken,mensagemController.enviaMensagem);
+//Rota para puxar dados de todos em minha lista de contato
+router.get("/MeusContatos", verificadorDoToken, contatoController.meusContatos)
 
-  //Rota para deletar/inativar uma mensagem enviada, para que não seja mais demonstrada ao outro usuário.
-  router.put("/DeletaMensagemEnviada", verificadorDoToken,mensagemController.deletaMensagemEnviada)
+//Rota para puxar as mensagens que tiveram entre o contato selecionado.
+router.get("/MensagensContato/:IDContato", verificadorDoToken, mensagemController.mensagensContato);
 
-  // rota para receber nossas notificações
-  router.get ("/Notificacoes", verificadorDoToken, notificacaoController.MinhasNotificacoes  )
 
-  //rota para marcar todas visualizações do usuário como vistas.
-  router.put("/MarcarNotificacoesVisto", verificadorDoToken, notificacaoController.marcarNotificacoesComoVisualizadas)
+//Rota para enviar uma mensagem para o contato selecionado
+router.post("/EnviaMensagem", verificadorDoToken, mensagemController.enviaMensagem);
 
-  //rota para o ADM BLOQUEAR UM USUÁRIO DO SISTEMA
-  router.post ("/BloquearUsuario", verificadorDoToken, UsuariosBloqueadosController.bloquearUmUsuario)
+//Rota para deletar/inativar uma mensagem enviada, para que não seja mais demonstrada ao outro usuário.
+router.put("/DeletaMensagemEnviada", verificadorDoToken, mensagemController.deletaMensagemEnviada)
 
-  // rota para qualquer usuário denunciar uma postagem
-  router.post ("/DenunciarPostagem", verificadorDoToken, denunciaController.denunciarPostagem)
+// rota para receber nossas notificações
+router.get("/Notificacoes", verificadorDoToken, notificacaoController.MinhasNotificacoes)
 
-  //Rota para criar um novo post
-  router.post ("/CriarPostagem", verificadorDoToken, postagemController.criaPostagem)
+//rota para marcar todas visualizações do usuário como vistas.
+router.put("/MarcarNotificacoesVisto", verificadorDoToken, notificacaoController.marcarNotificacoesComoVisualizadas)
 
-  //Rota para editar os dados da postagem que o usuário fez.
-  router.put ("/EditarPostagem", verificadorDoToken, postagemController.editarPostagem)
+//rota para o ADM BLOQUEAR UM USUÁRIO DO SISTEMA
+router.post("/BloquearUsuario", verificadorDoToken, UsuariosBloqueadosController.bloquearUmUsuario)
 
-  //Rota para puxar as ultimas 50 postagens feitas por usuarios, (solicita de 50 em 50)
-  router.post ("/VerPostagens", verificadorDoToken, postagemController.verPostagens)
+// rota para qualquer usuário denunciar uma postagem
+router.post("/DenunciarPostagem", verificadorDoToken, denunciaController.denunciarPostagem)
 
-  //Rota para reagir/avaliar uma postagem com "like", "uau" ou "amei"
-  router.post ("/ReagirPostagem", verificadorDoToken, avaliacaoController.reagirPostagem)
+//Rota para criar um novo post
+router.post("/CriarPostagem", verificadorDoToken, postagemController.criaPostagem)
 
-  //rota para retirar uma reação de uma postagem
-  router.delete ("/RemoverReacao/:IDPostagem", verificadorDoToken, avaliacaoController.removerReacao)
+//Rota para editar os dados da postagem que o usuário fez.
+router.put("/EditarPostagem", verificadorDoToken, postagemController.editarPostagem)
 
-  //rota para efetuar um comentário em algum post
-  router.post ("/comentarPost", verificadorDoToken, comentarioController.comentarEmUmPost)
+//Rota para puxar as ultimas 50 postagens feitas por usuarios, (solicita de 50 em 50)
+router.post("/VerPostagens", verificadorDoToken, postagemController.verPostagens)
 
-  //
+//Rota para reagir/avaliar uma postagem com "like", "uau" ou "amei"
+router.post("/ReagirPostagem", verificadorDoToken, avaliacaoController.reagirPostagem)
+
+//rota para retirar uma reação de uma postagem
+router.delete("/RemoverReacao/:IDPostagem", verificadorDoToken, avaliacaoController.removerReacao)
+
+//rota para efetuar um comentário em algum post
+router.post("/comentarPost", verificadorDoToken, comentarioController.comentarEmUmPost)
+
+//
 
 module.exports = router;
